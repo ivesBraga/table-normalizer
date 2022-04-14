cc3b6c6ea99e436e760e64930c6a87b4029c16a9
Ives Braga <ivesmoraisbraga@gmail.com>
2022-04-14 19:58:59 -0300

    fix: concerta erro req 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT
+    user.username AS usuario,
+    IF(TIMEDIFF(MAX(history.date_listened), "2020-12-31 23:59::59") > 0, "Usuário ativo", "Usuário inativo") "condicao_usuario"
+FROM
+    user as user
+    INNER JOIN
+    playback_history as history ON history.user_id = user.user_id
+GROUP BY user.username; 
