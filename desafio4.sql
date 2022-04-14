f3f7a75956793d0c79f1fc5efb7f52196cca8b9f
Ives Braga <ivesmoraisbraga@gmail.com>
2022-04-14 19:59:43 -0300

    fix: agr sim resolve erro req 4

--- desafio4.sql
+++ desafio4.sql
@@ -2,7 +2,7 @@ SELECT
     user.username AS usuario,
     IF(TIMEDIFF(MAX(history.date_listened), "2020-12-31 23:59::59") > 0, "Usuário ativo", "Usuário inativo") "condicao_usuario"
 FROM
-    user as user
+    user
     INNER JOIN
     playback_history as history ON history.user_id = user.user_id
 GROUP BY user.username; 
