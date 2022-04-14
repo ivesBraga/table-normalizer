9dc296031cfd8bdf6ef000e698ef546cb326cc61
Ives Braga <ivesmoraisbraga@gmail.com>
2022-04-14 20:43:21 -0300

    fix: :tada: resolve bug do req 11
    Agora sim conclui o projeto

--- desafio11.sql
+++ desafio11.sql
@@ -1,20 +1,20 @@
 SELECT song_name AS nome_musica, 
-  REPLACE(song, 'Streets', 'Code Review') AS novo_nome 
+  REPLACE(song_name, 'Streets', 'Code Review') AS novo_nome 
   FROM SpotifyClone.songs WHERE song_name LIKE '%Streets%'
 UNION
 SELECT song_name AS nome_musica, 
-  REPLACE(song, 'Her Own', 'Trybe') AS novo_nome 
+  REPLACE(song_name, 'Her Own', 'Trybe') AS novo_nome 
   FROM SpotifyClone.songs WHERE song_name LIKE '%Her Own%'
 UNION
 SELECT song_name AS nome_musica, 
-  REPLACE(song, 'Inner Fire', 'Project') AS novo_nome 
+  REPLACE(song_name, 'Inner Fire', 'Project') AS novo_nome 
   FROM SpotifyClone.songs WHERE song_name LIKE '%Inner Fire%'
 UNION
 SELECT song_name AS nome_musica, 
-  REPLACE(song, 'Silly', 'Nice') AS novo_nome 
+  REPLACE(song_name, 'Silly', 'Nice') AS novo_nome 
   FROM SpotifyClone.songs WHERE song_name LIKE '%Silly%'
 UNION
 SELECT song_name AS nome_musica, 
-  REPLACE(song, 'Circus', 'Pull Request') AS novo_nome 
+  REPLACE(song_name, 'Circus', 'Pull Request') AS novo_nome 
   FROM SpotifyClone.songs WHERE song_name LIKE '%Circus%'
 ORDER BY nome_musica; 
